c4a4c75c3594a0380c6d173b6bd12230709d48b8
Exanite <exanite78012@gmail.com>
2024-04-07 00:53:23 -0400

    Update project to use CodeName.Styling

---
+++ MatchConditions/AbilityMatchCondition.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using CodeName.Styling.Attributes;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace CodeName.Styling.MatchConditions
+{
+    [Obsolete("Use ParentSelectorMatchCondition instead")]
+    [Serializable]
+    public class AbilityMatchCondition : IMatchCondition
+    {
+        [HorizontalGroup] [HideLabel]
+        [StyleClassValueDropdown]
+        [SerializeField] private string styleClass;
+
+        [ShowInInspector]
+        [HideLabel] private string Name => "AbilityMatch";
+
+        public string StyleClass
+        {
+            get => styleClass;
+            set => styleClass = value;
+        }
+
+        public bool IsMatch(IStyleClassNode node)
+        {
+            var parent = node.Parent;
+            while (parent != null)
+            {
+                if (parent.HasClass("Ability"))
+                {
+                    return parent.HasClass(StyleClass);
+                }
+
+                parent = parent.Parent;
+            }
+
+            return false;
+        }
+    }
+}
